ef32102d5feb67d872c6932bc59fe7d4828e0d6c
Martynas <122054752+Martynnka@users.noreply.github.com>
2026-01-24 17:13:55 +0200

    Update PlayerMovement.cs
    Done with PlayerMovement.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float moveSpeed = 6f;
+    public float gravity = -20f;
+    public float jumpHeight = 1.2f;
+
+    public float rotationSpeed = 180f;
+
+    private CharacterController controller;
+    private float velocityY;
+
+    void Awake()
     {
-        
+        controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float turn = 0f;
+        if (Input.GetKey(KeyCode.A)) turn = -1f;
+        if (Input.GetKey(KeyCode.D)) turn = 1f;
+
+        transform.Rotate(0f, turn * rotationSpeed * Time.deltaTime, 0f);
+
+        float forwardInput = 0f;
+        if (Input.GetKey(KeyCode.W)) forwardInput = 1f;
+        if (Input.GetKey(KeyCode.S)) forwardInput = -1f;
+
+        Vector3 move = transform.forward * forwardInput * moveSpeed;
+
+        if (controller.isGrounded && velocityY < 0f)
+            velocityY = -2f;
+
+        if (controller.isGrounded && Input.GetButtonDown("Jump"))
+            velocityY = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+        velocityY += gravity * Time.deltaTime;
+
+        move.y = velocityY;
+
+        controller.Move(move * Time.deltaTime);
     }
 }
